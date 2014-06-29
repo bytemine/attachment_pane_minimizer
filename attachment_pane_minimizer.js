a570d3ea0dc5d5a9b58730d7decf7f1004657659
Daniel Rauer <rauer@bytemine.net>
2014-06-30 01:16:22 +0200

    Allow width adjustment of attachment panel.
    The width of the attachment panel on compose action can be
    adjusted by the user.
    
    The default to hide the attachment panel has been removed,
    the checkbox to display the panel also.

--- attachment_pane_minimizer.js
+++ attachment_pane_minimizer.js
@@ -1,19 +1,17 @@
-// display the attachment section in composer
-function showAttachmentSection()
-{
-  if($('#compose-attachments').css('display') == 'none')
-  {
-    // display attachment section
-    $('#compose-attachments').show();
-    $('#composebodycontainer').css('right', '260px');
-    $(window).resize();
-    $("#show_attachments").attr("disabled", true);
-  }
-}
-
 $(document).ready(function()
 {
-  $('#compose-attachments').hide();
-  $('#composebodycontainer').css('right', '0px');
-  $(window).resize();
+  $("#compose-attachments").resizable({ 
+    handles: "w",
+    stop: function(e, ui) {
+      $('#composebodycontainer').css('right', $('#compose-attachments').css('width'));
+      $(window).resize();
+    }
+  });
+  $("#compose-attachments").find("div.ui-resizable-w").addClass("ui-icon");
+  $("#compose-attachments").find("div.ui-resizable-w").addClass("ui-icon-arrowthick-2-e-w");
+  $("#compose-attachments").find("div.ui-resizable-w").css('cursor', 'w-resize');
+  $("#compose-attachments").find("div.ui-resizable-w").css('height', '15px');
+  $("#compose-attachments").find("div.ui-resizable-w").css('left', '-2px');
+  $("#compose-attachments").find("div.ui-resizable-w").css('top', '12px');
+  $("#compose-attachments").find("div.ui-resizable-w").css('width', '14px');
 });
